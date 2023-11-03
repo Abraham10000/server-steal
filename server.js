2561855f96752a6287a612221257f60dec976986
Abraham10000 <hei.abraham.4@gmail.com>
2023-11-03 21:28:48 +0200

    fix : repair server

--- server.js
+++ server.js
@@ -1,14 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
-const cors = require('cors'); 
 const port = 4000;
-
-// Middleware pour traiter les données JSON des requêtes POST
-app.use(bodyParser.json());
+const cors = require('cors');
 
 app.use(cors());
 
+// Middleware pour traiter les données JSON des requêtes POST
+app.use(bodyParser.json());
 
 // Variable pour stocker les informations obtenues dans /login
 const loginData = [];
@@ -18,17 +17,15 @@ app.post('/login', (req, res) => {
   // Récupérez les données du corps de la requête
   const { cliId, username, password } = req.body;
 
-  // Vérifiez si clientId, username et password sont non nuls
-  if (cliId && username && password) {
-    // Enregistrez les données dans la variable loginData si nécessaire
-    loginData.push({ cliId, username, password });
+  // Enregistrez les données dans la variable loginData
+  loginData.push({ cliId, username, password });
+
+  if (username && password) {
     res.status(200).json({ message: 'Authentification réussie' });
   } else {
     res.status(401).json({ message: 'Authentification échouée' });
   }
 });
-
-
 
 // Endpoint pour gérer les requêtes PATCH /ping
 app.patch('/ping', (req, res) => {
@@ -40,7 +37,7 @@ app.patch('/ping', (req, res) => {
 
 // Endpoint pour gérer les requêtes GET /
 app.get('/', (req, res) => {
-
+ 
   res.json({ message: 'Données de l\'application Express.js', loginData });
 });
 
@@ -48,3 +45,7 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`Serveur Express en cours d'exécution sur le port ${port}`);
 });
+
+
+// Export the Express API
+module.exports = app
